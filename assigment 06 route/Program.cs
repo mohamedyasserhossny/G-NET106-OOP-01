ad8f8da7d292586948c4ee6270e834c8c197f283
mohamedyasserhossny <my226546@gmail.com>
2026-09-12 17:33:25 -0700

    Add project files.

---
+++ assigment 06 route/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assigment_06_route
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
