ef24a3b134cd6da2ca81709152230b986277947a
mohamedyasserhossny <my226546@gmail.com>
2026-09-12 18:42:10 -0700

    question 3

---
+++ assigment 06 route/DeliveryAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigment_06_route
+{
+    internal class DeliveryAddress
+    {
+        public string? city;
+        public string? street;
+        public int buldingnumber;
+        public DeliveryAddress()
+        {
+            city = city;
+            street= street;
+            buldingnumber= buldingnumber;
+        }
+        public override string ToString()
+        {
+            return $"{city},{street},{buldingnumber}";
+        }
+    }
+     
+    }
