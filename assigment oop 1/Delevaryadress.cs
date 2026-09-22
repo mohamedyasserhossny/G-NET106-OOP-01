7bee2097dc43691a64b5e91c9b824d6b7d8a6962
mohamedyasserhossny <my226546@gmail.com>
2026-09-22 11:46:08 -0700

    part2 question 1

---
+++ assigment oop 1/Delevaryadress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigment_oop_1
+{
+    internal struct Delevaryadress
+    {
+        public string city;
+        public string street;
+        public int buldingnumber;
+        public Delevaryadress(string City, string Street, int Buldingnumber)
+        {
+            city = City;
+            street= Street;
+            buldingnumber= Buldingnumber;
+        }
+        public string Getfulladdrees()
+        {
+            return $"city={city},street={street},buldingnumber={buldingnumber}";
+        }
+
+    }
+}
--- assigment oop 1/Program.cs
+++ assigment oop 1/Program.cs
@@ -10,12 +10,22 @@
             #endregion
             #region question2 
             // a) 1- any one can use them dirctly
-                  // 2- no validtion 
-                  // 3- can not have control on them 
-             // b) 1- not any one can use dirctly
-                  // 2- validation 
-                  //3- control on them 
+            // 2- no validtion 
+            // 3- can not have control on them 
+            // b) 1- not any one can use dirctly
+            // 2- validation 
+            //3- control on them 
             #endregion
+            #region part2 question 1
+            Delevaryadress d1 = new Delevaryadress("tttt", "ccc", 2);
+            Console.WriteLine(d1.Getfulladdrees());
+            Delevaryadress d2 = d1;
+            d2.city = "rrr";
+            d2.street = "yyy";
+            d2.buldingnumber = 4;
+            Console.WriteLine(d1.Getfulladdrees()); 
+            #endregion
+
         }
     }
 }
