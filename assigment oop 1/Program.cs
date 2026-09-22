7f367bf75dce300fc4ee5eeb41b36d24e7ba203e
mohamedyasserhossny <my226546@gmail.com>
2026-09-22 14:18:55 -0700

    part2 system

--- assigment oop 1/Program.cs
+++ assigment oop 1/Program.cs
@@ -16,14 +16,61 @@
             // 2- validation 
             //3- control on them 
             #endregion
-            #region part2 question 1
-            Delevaryadress d1 = new Delevaryadress("tttt", "ccc", 2);
-            Console.WriteLine(d1.Getfulladdrees());
-            Delevaryadress d2 = d1;
-            d2.city = "rrr";
-            d2.street = "yyy";
-            d2.buldingnumber = 4;
-            Console.WriteLine(d1.Getfulladdrees()); 
+            //#region part2 question 1
+            //Delevaryadress d1 = new Delevaryadress("tttt", "ccc", 2);
+            //Console.WriteLine(d1.Getfulladdrees());
+            //Delevaryadress d2 = d1;
+            //d2.city = "rrr";
+            //d2.street = "yyy";
+            //d2.buldingnumber = 4;
+            //Console.WriteLine(d1.Getfulladdrees()); 
+            //#endregion
+            #region part2 system
+            delevarycenter delvarycenter = new delevarycenter();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("enter tarckingcode");
+                string trackingcode = Console.ReadLine();
+                Console.WriteLine("enter description");
+                string description = Console.ReadLine();
+                Console.WriteLine("enter the weight");
+                decimal weight = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("enter delevary fee");
+                decimal delevaryfee = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("enter city");
+                string city = Console.ReadLine();
+                Console.WriteLine("enter the street");
+                string street = Console.ReadLine();
+                Console.WriteLine("enter the bulding number");
+                int buldingnumber = int.Parse(Console.ReadLine());
+
+                Delevaryadress destiontion = new Delevaryadress(city, street, buldingnumber);
+                Shipment shipment = new Shipment(trackingcode, description, weight, delevaryfee, destiontion);
+                bool aded = delvarycenter.addshipment(shipment);
+                if (aded)
+                {
+                    Console.WriteLine("succefull");
+                }
+                else
+                {
+                    Console.WriteLine("failed");
+                }
+            }
+            for (int i=0;i<3;i++)
+            {
+                delvarycenter[i].printshipment();
+            }
+            Console.WriteLine("enter tracking code for search");
+            string searchcode = Console.ReadLine();
+            Shipment foundshipment = delvarycenter[searchcode];
+            if (foundshipment.trackingcode!=null)
+            {
+                foundshipment.printshipment();
+            }
+            else
+            {
+                Console.WriteLine("not found ");
+            }
             #endregion
 
         }
---
+++ assigment oop 1/delevarycenter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigment_oop_1
+{
+    internal struct delevarycenter
+    {
+        private Shipment[] shipments;
+        public delevarycenter()
+        {
+            shipments = new Shipment[10];
+        }
+        public Shipment this[int index ]
+        {
+            get
+            {
+                if (index>=0&&index<shipments.Length)
+                {
+                    return shipments[index];
+                }
+                return default;
+                
+            }
+            set
+            {
+                if (index >= 0 && index < shipments.Length)
+                    shipments[index] = value;
+            }
+        }
+        public Shipment this[string trackingcode]
+        {
+            get
+            {
+                for(int i=0;i<shipments.Length;i++)
+                {
+                    if (shipments[i].trackingcode==trackingcode)
+                    {
+                        return shipments[i];
+                    }
+                }
+                return default;
+            }
+        }
+        public bool addshipment(Shipment shipment)
+        {
+            for(int i=0;i<shipments.Length;i++)
+            {
+                if (shipments[i].trackingcode==null)
+                {
+                    shipments[i] = shipment;
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+
+    }
+}
